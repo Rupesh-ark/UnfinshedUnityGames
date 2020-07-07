e9d0df082f02249b0f78339ce714ca4c7f61c849
Rupesh Pandey <pandeyrupesh00@gmail.com>
2020-07-07 23:42:26 +0530

    Animation Applied and coded
    Player now has animation states.

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -19,6 +19,8 @@ public class CharacterController : MonoBehaviour
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
 
+
+
 	[Header("Events")]
 	[Space]
 
@@ -70,6 +72,7 @@ public class CharacterController : MonoBehaviour
 			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
 			{
 				crouch = true;
+				
 			}
 		}
 
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,39 +5,56 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController controller;
+    public CharacterController controller;   // A reference for the OG controller script basically allowing us to use it's funtions
 
-    private float horizontalMove;
+    private float horizontalMove;           // A variable to store the axis value of horizontal movement (-1 or 1) (aka left or right)(there is one more version of it called getaxis, read about it )
+    private bool jump = false;              // A variable to keep track of whether we are jumping or not
+    private bool crouch = false;            // A variable to keep track of whether we are crouching or not
+    float runSpeed = 40f;                   // A multiplier for movement
 
-    private bool jump = false;
-
-    private bool crouch = false;
-
-    float runSpeed = 40f;
+    public Animator animator;
+    
 
     // Update is called once per frame
     void Update()
     {
 
+        
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetButtonDown("Jump"))
+        animator.SetFloat("speed",Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+
+            animator.SetBool("isJumping", true);
+            
         }
 
-        if(Input.GetKeyDown(KeyCode.S))
+        if(Input.GetKeyDown(KeyCode.S))    // Will make a dedicated button in unity for crouching like there is one for jump(the code above)
         {
             crouch = true;
+           
         } else if(Input.GetKeyUp(KeyCode.S))
         {
             crouch = false;
+            
         }
 
 
 
     }
 
+    public void Onlanding()
+    {
+        animator.SetBool("isJumping", false);
+
+    }
+
+
+    //This function is called a fixed amount of time, Basically every 0.02 seconds so that the movement is done regardless of frame rate.
     private void FixedUpdate()
     {
 
